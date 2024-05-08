a8e39c696016329011a31644a2e2860516a0ab36
Nawaz Ahmed <ahmednawaz1810@gmail.com>
2024-05-08 15:35:51 +0530

    First commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                How about this for a footer:
+
+"Explore more movies: Discover the latest releases and timeless classics. Join our community: Stay updated on exclusive screenings, events, and behind-the-scenes insights. Connect with us: Follow us on social media for daily movie recommendations and discussions. © moviez 2024. All rights reserved."
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
